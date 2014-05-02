5479ae1d58527ce0c39de8683adfac22ae6c6de7
shines77 <gz_shines@msn.com>
2014-05-02 15:29:24 +0800

    more depends libs.
    Signed-off-by: shines77 <gz_shines@msn.com>

---
+++ spring_mvc3/src/net/i77soft/dbutils/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 基于 DbUtils 和 EhCache(java) 的数据库管理包
+ */
+/**
+ * @author shines77
+ * @date 2013-09-19
+ */
+package net.i77soft.dbutils;
---
+++ spring_mvc3/src/net/i77soft/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author shines77
+ * @date 2013-09-19
+ */
+package net.i77soft;
